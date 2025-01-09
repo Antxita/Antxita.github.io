9925a50400c77bb4e9398288e3d6985cc52ea6c1
Rafael <campossepulvedar@gmail.com>
2025-01-09 14:02:09 -0300

    feat: add section watermarks

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
+function obtenerBannerCercano() {
+    const banners = document.querySelectorAll('.banner');
+    let bannerCercano = null;
+    let distanciaMinima = Number.MAX_VALUE;
+
+    // Recorrer todos los banners
+    banners.forEach(banner => {
+        const rect = banner.getBoundingClientRect();
+
+        // Verificar si el banner está por encima de la vista del usuario
+        if (rect.bottom < 0) {
+            // Si está por encima, calcular la distancia desde el borde superior de la ventana
+            const distancia = Math.abs(rect.bottom); // Distancia desde el borde superior del viewport
+
+            // Verificar si este banner está más cerca que el anterior encontrado
+            if (distancia < distanciaMinima) {
+                distanciaMinima = distancia;
+                bannerCercano = banner;
+            }
+        }
+    });
+
+    // Retornar el ID del banner más cercano
+    return bannerCercano ? bannerCercano.id : null;
+}
 document.getElementById('down-button-1').addEventListener('click', function() {
     var destino = document.getElementById('content-1');
     destino.scrollIntoView({
@@ -30,4 +55,18 @@ window.addEventListener('scroll', function() {
         document.getElementById('upButton').classList.remove('show');
     }
 });
-
+window.addEventListener('scroll', function() {
+    var banner = document.getElementById(obtenerBannerCercano());
+    var watermarkSection = document.getElementById('watermark-section');
+    var watermarkDiv = document.getElementById('watermark-div');
+    if(banner != null){
+        if (banner.id = "ccdoc-banner-section"){
+            watermarkDiv.innerHTML="<hr style='display: inline-block; width: 20vh; margin-bottom: .2em;margin-right: 3vh; opacity: 20%;'><span id='watermark-text' style='font-weight: 300; font-size: small;'>CORPORACIÓN CHILENA DEL DOCUMENTAL</span>";
+        }else if (banner.id = "miradoc-banner-section"){
+            watermarkDiv.innerHTML="MIRADOC ESTRENOS";
+        }
+        watermarkSection.style.opacity = "80%";
+    }else{
+        watermarkSection.style.opacity = "0%";
+    }
+});
