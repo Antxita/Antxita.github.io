769079af2113f416a7a01211c9a1fc06d8e9103a
Rafael <campossepulvedar@gmail.com>
2025-01-15 13:21:59 -0300

    feat: add sections to miradoc

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
+
 function obtenerBannerCercano() {
-    const banners = document.querySelectorAll('.banner');
+    var banners = document.querySelectorAll('.banner');
     let bannerCercano = null;
     let distanciaMinima = Number.MAX_VALUE;
-
     // Recorrer todos los banners
     banners.forEach(banner => {
         const rect = banner.getBoundingClientRect();
@@ -11,7 +11,6 @@ function obtenerBannerCercano() {
         if (rect.bottom < 0) {
             // Si está por encima, calcular la distancia desde el borde superior de la ventana
             const distancia = Math.abs(rect.bottom); // Distancia desde el borde superior del viewport
-
             // Verificar si este banner está más cerca que el anterior encontrado
             if (distancia < distanciaMinima) {
                 distanciaMinima = distancia;
@@ -86,9 +85,9 @@ window.addEventListener('scroll', function() {
     var watermarkDiv = document.getElementById('watermark-div');
     var areBannersVisibleBool = areBannersVisible();
     if(banner != null && !areBannersVisibleBool){
-        if (banner.id = "ccdoc-banner-section"){
+        if (banner.id == "ccdoc-banner-section"){
             watermarkDiv.innerHTML="<hr style='display: inline-block; width: 20vh; margin-bottom: .2em;margin-right: 3vh; opacity: 10%;'><span id='watermark-text' style='font-weight: 300; font-size: small;'>CORPORACIÓN CHILENA DEL DOCUMENTAL</span>";
-        }else if (banner.id = "miradoc-banner-section"){
+        }else if (banner.id == "miradoc-banner-section"){
             watermarkDiv.innerHTML="<hr style='display: inline-block; width: 20vh; margin-bottom: .2em;margin-right: 3vh; opacity: 10%;'><span id='watermark-text' style='font-weight: 300; font-size: small;'>MIRADOC</span>";
         }
         watermarkSection.style.opacity = "100%";
